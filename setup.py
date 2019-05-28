ae4730c61e52f286bb2536eb01fbf482ab6bc35a
sblack4 <genseb7@gmail.com>
2019-05-28 15:05:37 -0400

    setup project

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Finetune TDD OOP Exercise',
+    version='1.0.1',
+    description='TDD and OOP with Gherkin and Python',
+    long_description=open('README.md').read(),
+    author='sblack4',
+    packages=['school_manager']
+)
